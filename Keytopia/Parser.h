a390faba9c7d8d403035fe658847a9b8dd064899
Rob Stearn <rob@Titan.local>
2015-05-08 14:08:39 +0100

    Added a Parser to handle translation between future persistence objects and model objects.

---
+++ Keytopia/Parser.h
@@ -0,0 +1,13 @@
+//
+//  Parser.h
+//  Keytopia
+//
+//  Created by Rob Stearn on 08/05/2015.
+//  Copyright (c) 2015 cocoadelica. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Parser : NSObject
+
+@end
---
+++ Keytopia/Parser.m
@@ -0,0 +1,13 @@
+//
+//  Parser.m
+//  Keytopia
+//
+//  Created by Rob Stearn on 08/05/2015.
+//  Copyright (c) 2015 cocoadelica. All rights reserved.
+//
+
+#import "Parser.h"
+
+@implementation Parser
+
+@end
