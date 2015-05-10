bf09848d99172bac1bb532fd9e58a131e02b72da
Rob Stearn <robstearn@me.com>
2015-05-10 14:26:29 +0100

    The barest start of the photos list

---
+++ Keytopia/PhotoLibraryCollection.h
@@ -0,0 +1,13 @@
+//
+//  PhotoLibraryCollection.h
+//  Keytopia
+//
+//  Created by Rob Stearn on 10/05/2015.
+//  Copyright (c) 2015 cocoadelica. All rights reserved.
+//
+
+@import UIKit;
+
+@interface PhotoLibraryCollection : UICollectionViewController
+
+@end
---
+++ Keytopia/PhotoLibraryCollection.m
@@ -0,0 +1,17 @@
+//
+//  PhotoLibraryCollection.m
+//  Keytopia
+//
+//  Created by Rob Stearn on 10/05/2015.
+//  Copyright (c) 2015 cocoadelica. All rights reserved.
+//
+
+#import "PhotoLibraryCollection.h"
+@import Photos;
+@import PhotosUI;
+
+@implementation PhotoLibraryCollection
+
+
+
+@end
